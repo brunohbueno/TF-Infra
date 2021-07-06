0f5d28fd666a8e7dc629fd7fea8549ccf8330b1e
Bruno <brunohebueno@gmail.com>
2021-07-06 19:42:29 -0300

    Update s3.py

--- s3.py
+++ s3.py
@@ -6,12 +6,12 @@ from werkzeug.utils import secure_filename
 
 def s3_upload(source_file):
     source_filename = secure_filename(source_file.data.filename)
-    Connect to S3 and upload file.
+    #Connect to S3 and upload file.
     s3 = boto3.client(
         "s3",
-        #aws_access_key_id=app.config["AWS_KEY"],
-        #aws_secret_access_key=app.config["AWS_SECRET"],
-        #aws_session_token=app.config["AWS_SESSION"]
+        aws_access_key_id=app.config["AWS_KEY"],
+        aws_secret_access_key=app.config["AWS_SECRET"],
+        aws_session_token=app.config["AWS_SESSION"]
     )
     s3.put_object(
         Bucket=app.config["S3_BUCKET"],
